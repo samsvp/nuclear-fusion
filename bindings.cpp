4518d60650dc509051028059a0a49685f44910a2
Samuel Simplicio <samuelsimplicio5@gmail.com>
2022-05-15 13:58:19 -0300

    Added brush palettes

--- bindings.cpp
+++ bindings.cpp
@@ -3,6 +3,7 @@
 #include "include/window.hpp"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 using namespace pybind11::literals;
@@ -20,10 +21,11 @@ PYBIND11_MODULE(nuclear_fusion, m) {
         .def("set_color", static_cast<void (nf::Brush::*)(uint8_t, uint8_t, uint8_t, uint8_t)>(&nf::Brush::setColor))
         .def("draw_line", &nf::Brush::draw_line, "window"_a, "x1"_a, "y1"_a, "x2"_a, "y2"_a, "step"_a=0.01)
         .def("draw_circle", &nf::Brush::draw_circle, "window"_a, "x"_a, "y"_a, "r"_a, "step"_a=0.01)
-        .def("draw_ellipse", &nf::Brush::draw_ellipse, "window"_a, "x"_a, "y"_a, "rx"_a, "ry"_a, "step"_a=0.01);
+        .def("draw_ellipse", &nf::Brush::draw_ellipse, "window"_a, "x"_a, "y"_a, "rx"_a, "ry"_a, "step"_a=0.01)
+        .def("set_palette", static_cast<void (nf::Brush::*)(const std::vector<std::vector<uint8_t>>)>(&nf::Brush::set_palette));
 
     py::class_<nf::Vec2>(m, "Vec2")
         .def(py::init<float, float>())
-        .def("rotate", static_cast<nf::Vec2 (nf::Vec2::*)(float, const nf::Vec2&, bool)>(&nf::Vec2::rotate),
+        .def("rotate", static_cast<nf::Vec2 (nf::Vec2::*)(float, const nf::Vec2&, bool) const>(&nf::Vec2::rotate),
             "angle"_a, "center"_a=nf::Vec2{0,0}, "is_degree"_a=false);
 }
